2555cc3c115939e94a54a9717dbcd0b9f9f2b8aa
Daniel Khalil <dkses.hef@uea.edu.br>
2022-09-02 23:42:15 -0400

    Crud em flutter

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,30 @@
+//Dados mockados
+
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Taylor',
+      email: 'taylor@alunos.com.br',
+      avatarUrl:
+          'https://midias.correiobraziliense.com.br/_midias/png/2022/04/29/675x450/1_pasted_image_0__5_-7873336.png?20220429172734?20220429172734'),
+  '2': const User(
+      id: '2',
+      name: 'Robyn',
+      email: 'robyn@gmail.com',
+      avatarUrl:
+          'https://cdns-images.dzcdn.net/images/artist/1de7f8f949ad42080ecacc93c3b3d813/500x500.jpg'),
+  '3': const User(
+      id: '3',
+      name: 'Eilish',
+      email: 'beilish@yahoo.com',
+      avatarUrl:
+          'https://rollingstone.uol.com.br/media/_versions/billie-eilish-spotify-matt_winkelmeyer-getty_images_for_spotify_widelg.jpg'),
+  '4': const User(
+      id: '4',
+      name: 'Rae',
+      email: 'craejepsen@hotmail.com',
+      avatarUrl:
+          'https://yt3.ggpht.com/IBfcKxmUaL1OsqQVVtYQrXZYEipVx-PekP93te_Tpw2JJUXSlxzCcnDjQKR7GKfPaJ_cpmWFrnE=s900-c-k-c0x00ffffff-no-rj'),
+};
---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+//Classe modelo Usuario com atributos que serão utilizados
+
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String avatarUrl;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.avatarUrl,
+  });
+}
